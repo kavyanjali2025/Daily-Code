bd9d981f61183df002e5a2a83902efc478816541
Kishor Kalane <kishorkalane989@gmail.com>
2026-03-22 07:52:48 +0530

    Data Type Byte and Int

---
+++ 2026/March/22/Prog4.java
@@ -0,0 +1,6 @@
+class Prog4 {
+        public static void main(String[] args) {
+            int x = 2140000000;
+            System.out.println(x);
+        }  
+}
---
+++ 2026/March/22/Prog5.java
@@ -0,0 +1,6 @@
+class Prog5 {
+    public static void main(String[] args){
+        int x = -21432456;
+        System.out.println(x);
+    }   
+}
